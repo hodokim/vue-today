48eea5a49e80efac3131ae2dee5f819113c27b88
hodokim <hodo323@naver.com>
2021-11-19 23:34:32 +0900

    Vue Signup Page

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <!-- v-on:submit=""  === @submit=""!-->
+    <div>
+      <label for="username">id : </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw : </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <div>
+      <label for="nickname">nickname : </label>
+      <input id="nickname" type="text" v-model="nickname" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import { registerUser } from '@/api/index';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      nickname: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('FORM SUBMIT COMPLETE');
+      const userData = {
+        username: this.username,
+        password: this.password,
+        nickname: this.nickname,
+      };
+      registerUser(userData);
+    },
+  },
+};
+</script>
+
+<style></style>
