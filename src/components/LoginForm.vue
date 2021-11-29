6bb8fb3d33e927881d815ff1896383eb4dadfd46
hodokim <hodo323@naver.com>
2021-11-29 23:12:10 +0900

    Vue Token

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -55,7 +55,8 @@ export default {
           password: this.password,
         };
         const { data } = await loginUser(userData);
-        console.log(data.user.username);
+        console.log(data.token);
+        this.$store.commit('setToken', data.token);
         this.$store.commit('setUsername', data.user.username);
         this.$router.push('/main');
         //  this.logMessage = `${data.user.username} 님 환영합니다`;
