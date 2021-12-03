387ae2bfc170bcce4794953a6710485ae7231c4e
hodokim <hodo323@naver.com>
2021-12-03 23:03:59 +0900

    Vue Css and Component

---
+++ src/components/posts/PostListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <div class="post-title">
+      {{ postItem.title }}
+    </div>
+    <div class="post-contents">
+      {{ postItem.contents }}
+    </div>
+    <div class="post-time"></div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    postItems: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,18 +3,33 @@
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
       <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :item="postItem"
+        ></PostListItem>
+        <!-- <li v-for="postItem in postItems" :key="postItem._id">
+          <div class="post-title">
+            {{ postItem.title }}
+          </div>
+          <div class="post-contents">
+            {{ postItem.contents }}
+          </div>
+          <div class="post-time"></div>
+        </li> -->
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
+  components: {
+    PostListItem,
+  },
   data() {
     return {
       postItems: [],
