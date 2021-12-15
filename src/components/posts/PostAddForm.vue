9b405389705faeba4a9b410b30eb47acd9109900
hodokim <hodo323@naver.com>
2021-12-15 23:03:09 +0900

    Create Post

---
+++ src/components/posts/PostAddForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="contents">
+    <h1 class="page-header">Create Post</h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title:</label>
+          <input id="title" type="text" v-model="title" />
+        </div>
+        <div>
+          <label for="contents">Contents:</label>
+          <textarea id="contents" rows="5" v-model="contents" />
+        </div>
+        <button class="btn" type="submit">Create</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createPost } from '@/api/index';
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      const response = await createPost({
+        title: this.title,
+        contents: this.contents,
+      });
+      console.log(response);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+
+.btn {
+  color: white;
+}
+</style>
